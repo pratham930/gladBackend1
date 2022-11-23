362f22fbc98e158f76b47ed9089934b05a08cc40
Roshan <ranaroshan763@gmail.com>
2022-11-23 20:29:08 +0530

    socket install
    socket install

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import connectDB from './db/connectdb.js'
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 import userRoute from './routes/user.js'
 import staffRoute from './routes/staff.js'
@@ -14,6 +16,10 @@ const DATABASE_URL =
 // mongodb+srv://pratham:lookman17@cluster1.vuxzs.mongodb.net/?retryWrites=true&w=majority
 connectDB(DATABASE_URL)
 export const app = express()
+
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, { cors: { origin: '*' } });
 //  app.use(cookieParser())
 app.use(express.json())
 dotenv.config({ path: './config.env' })
@@ -26,6 +32,26 @@ app.use('/api', staffRoute)
 app.use('/api', sellerRoute)
 
 
-app.listen(port, () => {
+// app.listen(port, () => {
+//   console.log(`local host:${port}`)
+// })
+
+const server = httpServer.listen(port, () => {
   console.log(`local host:${port}`)
+})
+io.listen(server)
+
+
+io.on("connection", (socket) => {
+  // SOCKET CONNECTION ESTABLISHED
+  console.log("Connected to socket.io with id --> ", socket.id);
+  socket.emit("connected");
+
+
+  socket.on("setup", (userData) => {
+    //  socket.join(userData._id);
+    console.log(userData, "64")
+    socket.emit("connected")
+  })
+
 })
